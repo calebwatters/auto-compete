c71f6bae412f31bd8dbee2eac1b7f461358635b0
calebwatters <caleb@h2os.org>
2019-07-25 18:23:47 -0700

    built prelim files

---
+++ javascript.js
@@ -0,0 +1,13 @@
+const readline = require('readline');
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+    terminal: false
+});
+
+rl.on('line', function (line) {
+
+    console.log(line)
+
+
+})
